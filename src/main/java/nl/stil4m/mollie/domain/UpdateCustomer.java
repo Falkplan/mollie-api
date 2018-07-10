8a6ff69ca93c3ba41780f39dc5f76267a9278401
Casper Hollemans <casper.hollemans@route.nl>
2018-07-10 13:06:57 +0200

    Fixed issue with updating a customer

--- src/main/java/nl/stil4m/mollie/domain/UpdateCustomer.java
+++ src/main/java/nl/stil4m/mollie/domain/UpdateCustomer.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Optional;
 
 
-@JsonInclude(value = JsonInclude.Include.NON_DEFAULT, content = JsonInclude.Include.NON_EMPTY)
+@JsonInclude(content = JsonInclude.Include.NON_EMPTY)
 public class UpdateCustomer {
 
 
